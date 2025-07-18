f254b2c9cb728718842f9f30e1a9e01e0bfa2104
konrad2002 <konrad@schwimmteamerzgebirge.de>
2025-07-18 02:17:26 +0200

    add "konrad" text and github action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1 className={styles.mainTitle}>Konrad</h1>
     </div>
   );
 }
